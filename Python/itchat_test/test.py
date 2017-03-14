a02fe059f57a7f03aaf7c49a744659d04e9de7ea
ChenBooming <421235586@qq.com>
2017-03-14 15:40:31 +0800

    2017年03月14日15:40:28

--- Python/itchat_test/test.py
+++ Python/itchat_test/test.py
@@ -8,13 +8,15 @@ from tuling import get_response
 def text_reply(msg):
     if u'作者' in msg['Text'] or u'主人' in msg['Text']:
         return u'找陈蒙 421235586@qq.com'
-    elif u'同盟会群' in msg['Text'] or u'报名同盟会' in msg['Text']:
-        itchat.send('@img@test_group.jpeg', msg['FromUserName'])
-        return u'进群有问题请艾特我或者群主'
+    elif u'同盟会群' in msg['Text'] or u'同盟会' in msg['Text']:
+        itchat.send('@img@/Users/koudai232/PycharmProjects/python_pycharm/Python/itchat_test/img/tongmenghui_group.png', msg['FromUserName'])
+        return u'长按扫码进群，有问题请@陈蒙或者群主'
     elif u'亲子活动' in msg['Text']:
-        itchat.send('@img@funmili.jpg', msg['FromUserName']) # there should be a picture
+        itchat.send('@img@/Users/koudai232/PycharmProjects/python_pycharm/Python/itchat_test/img/funbailing.jpg', msg['FromUserName']) # there should be a picture
+        return u'长按扫码关注，有问题联系@陈蒙'
     elif u'白领活动' in msg['Text']:
-        itchat.send('@img@funbailing.bmp', msg['FromUserName'])  # there should be a picture
+        itchat.send('@img@/Users/koudai232/PycharmProjects/python_pycharm/Python/itchat_test/img/funmili.jpg', msg['FromUserName'])  # there should be a picture
+        return u'长按扫码关注，有问题联系@陈蒙'
     else:
         return get_response(msg['Text']) or u'收到：' + msg['Text']
 
@@ -46,7 +48,7 @@ def add_friend(msg):
     itchat.add_friend(**msg['Text'])
     itchat.send_msg(u'项目主页：https://github.com/ChenBooming/python_pycharm/tree/master/Python/itchat_test\n'
         + u'同盟会入群  ：回复 同盟会群\n' + u'亲子活动：回复 亲子活动\n' + u'白领活动：回复 白领活动\n'
-        + u'有问题反馈： 回复 作者', msg['RecommendInfo']['UserName'])
+        + u'有问题反馈： 回复 作者，发送到坐着邮箱即可', msg['RecommendInfo']['UserName'])
 
 itchat.auto_login(True, enableCmdQR=False)
 itchat.run()
